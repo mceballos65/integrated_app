609e5c25c85dedeb5ef9743efeacae8334aa392e
Mauro Ezequiel Ceballos <Mauro.Ezequiel.Ceballos@kyndryl.com>
2025-07-17 14:19:30 -0600

    add all

---
+++ set_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+
+#  source ./set_env.sh
+
+export DX_EXT_CFG_GIT_REPO="https://github.com/mceballos65/integrated_app_config_2.git"
+export DX_ENV_OU_GSMA_CODE="ACM
+export DX_EXT_CFG_GIT_USER="mceballos65"
+export DX_EXT_CFG_GIT_TOKEN="ingresa valor aqui"
+export DX_EXT_GUI_USER="mauro"
+export DX_EXT_GUI_PASSWORD="!Passw0rd"
+
+echo "Variables de entorno seteadas. Verificar con: env | grep DX_"
